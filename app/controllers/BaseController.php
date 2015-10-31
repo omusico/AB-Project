e8b22c7256d95311ceed96b2aa970b5cddca3548
omusico <omusico@gmail.com>
2015-10-31 13:31:37 +0800

    add base ctrl

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,6 +1,17 @@
 <?php
+use Illuminate\Routing\Controller;
+
 class BaseController extends Controller {
 	public function __construct()
 	{
+	}
+	
+	protected function setupLayout()
+	{
+		if ( ! is_null($this->layout))
+		{
+			$this->layout = View::make($this->layout);
+		}
+		View::share('currentUser', Auth::user());
 	}
 }
--- app/models/User.php
+++ app/models/User.php
@@ -1,4 +1,6 @@
 <?php
+
+use Illuminate\Database\Eloquent;
 class User extends Eloquent{
 	
 }
